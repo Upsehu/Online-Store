e0c6fa854412130970528f040e15b63639b32e98
Rishabh Kamboj <heyrishabh13@gmail.com>
2021-10-19 15:58:47 +0530

    First Commit

---
+++ components/navbar.js
@@ -0,0 +1,20 @@
+function navbar(){
+
+    return `<div id="navbar">
+
+    <h3><a href="/">Home</a></h3>
+
+    <div id="options">
+        <h3><a href="jewellery.html">Jewllery</a></h3>
+        <h3><a href="electronics.html">Electronics</a></h3>
+        <h3><a href="#">SignIn</a></h3>
+        <h3><a href="#">SignUp</a></h3>
+        <!-- <h3><a href="jewwllery.html"></a></h3> -->
+
+    </div>
+</div>`;
+}
+
+
+export default navbar
+
